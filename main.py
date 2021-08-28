54572c42b9d8dc9547c3a549ad3a552e0744f137
sam505 <mainasamuel505@gmail.com>
2021-08-28 23:28:14 +0300

    Adding gaussian files

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
-from keras.models import load_model
-
-model = load_model("/home/maina/PycharmProjects/Predicting_Stock_Prices/Stock_Prices_Prediction.model")
-
-print(model.input_shape)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
+    """
+
+    :return:
+    """
+
+    return None
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    main()
